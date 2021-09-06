235aca315bf296037de11a9fb554c9c744640e27
Martín A. Frangul <postguerra@gmail.com>
2021-09-06 19:55:24 +0200

    Separats per Nivells

---
+++ Nivell 3/javascript.js
@@ -0,0 +1,22 @@
+// Exercici 8
+
+function calculadora(op, x, y){
+    if (!isNaN(x) && !isNaN(y)) {
+      if (op == 'suma')
+          console.log(x + y)
+      else if (op == 'resta')
+          console.log(x - y)
+      else if (op == 'multiplicacion' || op == 'multiplicacion')
+          console.log(x * y)
+      else {(op == 'división' || op == 'division')
+            if (x === 0 && y === 0)
+               console.log('¡No es posible realizar esta operación!')
+            else console.log(x / y)
+           }
+    }
+  else {
+    console.log('Uno o más elementos no son válidos')
+  }
+}
+    
+var resultat = calculadora('resta', 9, 8)
