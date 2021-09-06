f8fae08ca816eba0b27318813f1c96dad8e2676c
Martín A. Frangul <postguerra@gmail.com>
2021-09-06 19:25:43 +0200

    JS acabat

--- Nivell 1/javascript.js
+++ Nivell 1/javascript.js
@@ -1,8 +1,22 @@
 // Exercici 8
-function calculadora(suma, x, y){
-    x = prompt('Esciba un número')
-    y = prompt('Otro')
-    suma = (x + y)
-}
 
-console.log(calculadora(suma))
+function calculadora(op, x, y){
+    if (!isNaN(x) && !isNaN(y)) {
+      if (op == 'suma')
+          console.log(x + y)
+      else if (op == 'resta')
+          console.log(x - y)
+      else if (op == 'multiplicacion' || op == 'multiplicacion')
+          console.log(x * y)
+      else {(op == 'división' || op == 'division')
+            if (x === 0 && y === 0)
+               console.log('¡No es posible realizar esta operación!')
+            else console.log(x / y)
+           }
+    }
+  else {
+    console.log('Uno o más elementos no son válidos')
+  }
+}
+    
+var resultat = calculadora('resta', 9, 8)
