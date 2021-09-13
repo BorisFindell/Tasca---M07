2ccdbb4205a89cfc13f8d9aa399e788cf9fb48d4
Martín A. Frangul <postguerra@gmail.com>
2021-09-13 16:23:55 +0200

    Acabat calc func

--- Nivell 3/javascript.js
+++ Nivell 3/javascript.js
@@ -1,32 +1,127 @@
 // Exercici 8
 
-function calculadora(op, x, y){
-    if (!isNaN(x) && !isNaN(y)) {
-      if (op == 'suma')
-          console.log(x + y)
-      else if (op == 'resta')
-          console.log(x - y)
-      else if (op == 'multiplicacion' || op == 'multiplicacion')
-          console.log(x * y)
-      else {(op == 'división' || op == 'division')
-            if (x === 0 && y === 0)
+//VARIABLES
+
+var val1 = null;
+var val2 = null;
+var operador = null;
+var result = null;
+
+
+//LISTENERS
+
+document.getElementById("un").addEventListener("click", function(){
+  addNum(1);
+})
+document.getElementById("dos").addEventListener("click", function(){
+  addNum(2);
+})
+document.getElementById("tres").addEventListener("click", function(){
+  addNum(3);
+})
+document.getElementById("quatre").addEventListener("click", function(){
+  addNum(4);
+})
+document.getElementById("cinc").addEventListener("click", function(){
+  addNum(5);
+})
+document.getElementById("sis").addEventListener("click", function(){
+  addNum(6);
+})
+document.getElementById("set").addEventListener("click", function(){
+  addNum(7);
+})
+document.getElementById("vuit").addEventListener("click", function(){
+  addNum(8);
+})
+document.getElementById("nou").addEventListener("click", function(){
+  addNum(9);
+})
+document.getElementById("zero").addEventListener("click", function(){
+  addNum(0);
+})
+document.getElementById("sum").addEventListener("click", function(){
+  addOp('suma');
+})
+document.getElementById("rest").addEventListener("click", function(){
+  addOp('resta');
+})
+document.getElementById("mult").addEventListener("click", function(){
+  addOp('multiplicacion');
+})
+document.getElementById("div").addEventListener("click", function(){
+  addOp('division');
+})
+document.getElementById("clear").addEventListener("click", function(){
+  clear();
+})
+document.getElementById("igual").addEventListener("click", function(){
+  resultat(val1, operador, val2);
+})
+
+//FUNCIÓ ADDNUMBER
+
+function addNum(num) {
+  if (val1 == null || operador == null) {
+    if (val1 == null) {
+      val1 = val1 + num    
+      console.log(val1)
+    }
+    else {
+      val1 = val1 + "" + num
+      console.log(val1)
+    }
+  };
+  if (val1 != null && operador != null) {
+    if (val2 == null) {
+      val2 = val2 + num 
+      console.log(val2)
+    }  
+    else {
+      val2 = val2 + "" + num
+      console.log(val2)
+    }
+  }
+};
+
+
+//FUNCIÓN CLEAR
+
+function clear() {
+  val1 = null;
+  val2 = null;
+  operador = null;
+}
+
+
+//FUNCIÓN ADD OP
+
+function addOp(op) {
+  if (val1 != null) {
+    operador = op
+    console.log(operador)
+
+  }
+}
+//FUNCIÓ DE LA CALCULADORA
+
+function resultat(val1, operador, val2){
+    if (!isNaN(val1) && !isNaN(val2)){
+      if (operador == 'suma'){
+        result = (val1 + val2)
+          console.log(result)
+      }
+      else if (operador == 'resta')
+          console.log(val1 - val2)
+      else if (operador == 'multiplicacion' || operador == 'multiplicacion')
+          console.log(val1 * val2)
+      else {(operador == 'división' || operador == 'division')
+            if (val1 == 0 && val2 == 0)
                console.log('¡No es posible realizar esta operación!')
-            else console.log(x / y)
+            else console.log(val1 / val2)
            }
     }
   else {
     console.log('Uno o más elementos no son válidos')
   }
 }
-    
-var resultat = calculadora('resta', 9, 8)
-
-
-(function () {
-  var numbers = document.getElementByClass("numbers");
-  var operators = document.getElementByClass("operators");
-  var igual = document.getElementByClass("igual");
-
-  numbers.addEventListener("click", asignarValor, false);
-  operators.addEventListener("click", asignarOperador, false);
-  igual.addEventListener("click", calcular, false);
